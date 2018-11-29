dcb3c0b811af59b64bba6c12fdb91636191e9b7d
moka <gaoyuanfell@sina.com>
2018-11-29 16:08:15 +0800

    2018-11-29 16:08:14 → moka

--- git.sh
+++ git.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 echo "Start to publish..."
 git add .
-date=$(date '+%Y-%m-%d %H:%M:%S → moka') `msg → $1`
+date=$(date '+%Y-%m-%d %H:%M:%S → moka') `"msg →" $1`
 commit="git commit -am '"$date"'"
 eval $commit
 git pull origin master
